e548be3a2b673222a9a1088618a2b14afa31efb7
r567tw <r567tw@gmail.com>
2018-06-09 16:09:11 +0800

    mail

--- app/Http/Controllers/UserAuthController.php
+++ app/Http/Controllers/UserAuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Validator;
 use Hash;
+use Mail;
 use App\User;
 
 class UserAuthController extends Controller
@@ -41,6 +42,17 @@ class UserAuthController extends Controller
         $input['password'] = Hash::make($input['password']);
         $user = User::create($input);
         //send email to do
+        $mail_binding =[
+            'nickname' => $input['name']
+        ];
+
+        Mail::send('email.signUpEmailNotification',$mail_binding,function($mail) use ($input){
+            $mail->to($input['email']);
+            $mail->from('admin@shop-laravel.com');
+            $mail->subject('恭喜註冊shop-laravel 成功');
+        });
+
+        return redirect('/user/auth/sign-in');
     }
     
     // Route::get('/sign-in','UserAuthController@signInPage');
